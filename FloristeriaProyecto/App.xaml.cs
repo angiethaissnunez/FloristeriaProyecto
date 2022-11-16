74bba8abce44d8934800f2d69d560eed72c7ab61
angie nunez <angiethaiss52@gmail.com>
2022-11-15 21:01:27 -0600

    Add project files.

---
+++ FloristeriaProyecto/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace FloristeriaProyecto
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+
+           /* Device.SetFlags(new string[] {
+                "AppTheme_Experimental",
+                "MediaElement_Experimental"
+                });*/
+
+
+            InitializeComponent();
+           MainPage = new Views.PageLogin();
+
+           /* if (!Preferences.ContainsKey("usuarios"))
+            {
+                //MainPage = new Views.LoginView();
+                MainPage = new Views.PageInicio();
+            }
+            else
+            {
+                MainPage = new Views.PageInicio();
+            }*/
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ FloristeriaProyecto/AppSettings.cs
@@ -0,0 +1,28 @@
+﻿using AppNotas.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FloristeriaProyecto
+{
+    public class AppSettings
+    {
+        public static string ApiFirebase = "https://appfloristeria-default-rtdb.firebaseio.com/";
+        private static string KeyAplication = "AIzaSyC-O1SLFIfJsBOUi6kAGnXsX8FGhpnWDMA";
+
+
+        public static ResponseAuthentication oAuthentication { get; set; }
+        private static string ApiUrlGoogleApis = "https://identitytoolkit.googleapis.com/v1/";
+
+        public static string ApiAuthentication(string tipo)
+        {
+            if (tipo == "LOGIN")
+                return ApiUrlGoogleApis + "accounts:signInWithPassword?key=" + KeyAplication;
+            else if (tipo == "SIGNIN")
+                return ApiUrlGoogleApis + "accounts:signUp?key=" + KeyAplication;
+            else
+                return "";
+        }
+
+    }
+}
